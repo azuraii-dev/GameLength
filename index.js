18d9065b1abb77054f9a229096b07681573158d1
azuraii <sebastian.b2000@live.com>
2022-08-26 00:17:01 +1000

    implemented redis caching

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const cheerio =  require('cheerio')
 const express = require('express')
 const axios = require('axios')
 const qs = require('qs')
+const redis = require('redis')
+
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT
 const NODE_ENV = process.env.NODE_ENV
+const REDIS_ENABLED = NODE_ENV == "production";
+
+let redisClient = null;
+
+async() => {
+    if (REDIS_ENABLED) {
+        redisClient = redis.createClient();
+    
+        redisClient.on('error', function(err) {
+            console.log('Redis Client Error', err)
+        });
+    
+        await redisClient.connect("redis://:2rI51FLJ8bD#Rh%jA@127.0.0.1:6379");
+    }
+}
 
 const ERRORS = {
     fetchError: {error: 'Unable to fetch game data'},
@@ -24,8 +41,15 @@ app.get('/', async(req, res) => {
 
 app.get('/game/:query', async(req, res) => {
     const query = req.params.query || null;
+
     const result = await searchQuery(query)
-    res.json(result)
+
+    if (result.error) {
+        res.status(404)
+    } else {
+        res.json(result)
+    }
+
     res.end()
 })
 
@@ -37,12 +61,18 @@ app.get('*', async(req, res) => {
 app.listen(PORT, () => {
     console.log(`API listening on port ${PORT}`)
     console.log(`Environment set to ${NODE_ENV}`)
-  })
+})
 
 
 async function searchQuery(query) {
 
     if (query == null) return ERRORS.fetchError;
+
+    const cachedResult = await fetchCachedGame(query);
+
+    if (cachedResult) {
+        return cachedResult
+    }
 
     const res = await axios.post('https://howlongtobeat.com/search_results?page=1', 
     
@@ -75,9 +105,11 @@ async function searchQuery(query) {
     try {
         const $ = cheerio.load(res.data)
         const url = $('ul li').first().find('.shadow_text a').attr('href')
-        const gameLength = await getGameTime(`https://howlongtobeat.com/${url}`)
+        const gameData = await getGameTime(`https://howlongtobeat.com/${url}`)
 
-        return gameLength
+        await cacheGame(query, gameData)
+
+        return gameData
 
     } catch (error) {
 
@@ -87,6 +119,7 @@ async function searchQuery(query) {
 }
 
 async function getGameTime(url) {
+    
     const res = await axios.get(url)
 
     try {
@@ -100,13 +133,44 @@ async function getGameTime(url) {
 
         const appId = !steamUrl ? 0 : steamUrl.slice(0, -1).split('/').at(-1)
 
-        return {success: true, title: gameTitle, gameLength: gameLength, appId: appId}
+        const result = {success: true, title: gameTitle, gameLength: gameLength, appId: appId}
+
+        return result
     
     } catch (error) {
         
         return ERRORS.fetchError
     }
 
+}
+
+async function cacheGame(game, data) {
+    if (!REDIS_ENABLED) return;
+
+    await redisClient.set(game, JSON.stringify(data), {
+        EX: 3600,
+        NX: true,
+    });
+
+    console.log(game, "cached")
+}
+
+async function fetchCachedGame(game) {
+
+    if (!REDIS_ENABLED) return false;
+
+    try {
+        const cachedResult = await client.get(game);
+
+        if (cachedResult) {
+            console.log(game, "from cache")
+            return JSON.parse(cachedResult);
+        }
+        return false
+
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 
